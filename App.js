0ac27f2301029a196988070b2ae4c9c731c9fe91
Alexis Gilberth Renteria <gilberthr10@gmail.com>
2022-11-29 17:56:18 -0500

    my first React Native app

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, ScrollView, Platform } from 'react-native';
+import { dogs } from './data';
+const Card = ({name, description, image, size}) => {
+  // name
+  // description
+  // image
+  // size
+  return (
+    <View style={{
+      width: "100%",
+      height: 'auto',
+      backgroundColor: 'rgba(255, 255, 255,.82)',
+      boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+      backdropFilter:' blur(5px)',
+      padding: 10,
+      border: 'border: 1px solid rgba(255, 255, 255, 1)',
+      borderRadius: 30,
+      marginBottom: 30
+    }}>
+      <Image
+        style={{width: "100%", height: 420, borderRadius: 20}}
+        source={{ uri: image}}
+      />
+      <Text style={{fontSize: 28, fontWeight: 'bold', marginTop: 10}}>{name}</Text>
+      <Text style={{fontSize: 18, marginTop: 10, color: 'gray'}}>{description}</Text>
+    </View>
+  )
+}
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        contentContainerStyle={{marginHorizontal: 12,}}
+        style={{marginTop: Platform.OS === 'android' && 30}}>
+        {dogs.map((dog, index) => <Card key={index} {...dog}/>)}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'linear-gradient(180deg, rgba(224,233,253,1) 0%, rgba(233,236,241,1) 90%)',
   },
 });
---
+++ data.js
@@ -0,0 +1,22 @@
+export const dogs = [
+    {
+        name: "Pug",
+        description: "This is the description",
+        image: "https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
+    },
+    {
+        name: "Maylo",
+        description: "I like to run and play ball",
+        image: "https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    },
+    {
+        name: "Jeff",
+        description: "This is the description",
+        image: "https://images.unsplash.com/photo-1588943211346-0908a1fb0b01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
+    },
+    {
+        name: "Mr. Jumps",
+        description: "I like to surf on the beach",
+        image: "https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"
+    }, 
+];
